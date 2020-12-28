0895bf1fd40154140ed18b08ebf9aa1edafa5c24
Jet Fontanilla <jetlogs@gmail.com>
2020-12-29 01:23:09 +0800

    initial evidence UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EVIDENCE, Evidence, Ghost, NameDictionary } from './ghost-properties';
+import { isUndefined } from 'lodash-es';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Phasmophobia Ghost Guesser';
+  private evidence: (boolean | undefined)[] = [undefined, undefined, undefined, undefined, undefined, undefined];
+  private likelyCandidates: Ghost[] = [];
+  private sureCandidates: Ghost[] = [];
+
+  reset(): void {
+    this.evidence = [undefined, undefined, undefined, undefined, undefined, undefined];
+    this.likelyCandidates = [];
+    this.sureCandidates = [];
+  }
+
+  getEvidence(): NameDictionary[] {
+    return EVIDENCE;
+  }
+
+  isEvidenceUndefined(evidence: Evidence): boolean {
+    const currentEvidence = this.evidence[evidence];
+    return isUndefined(currentEvidence);
+  }
+
+  isEvidencePresent(evidence: Evidence): boolean {
+    const currentEvidence = this.evidence[evidence];
+    if (isUndefined(currentEvidence)) {
+      return false;
+    }
+    return currentEvidence === true;
+  }
+
+  isEvidenceAbsent(evidence: Evidence): boolean {
+    const currentEvidence = this.evidence[evidence];
+    if (isUndefined(currentEvidence)) {
+      return false;
+    }
+    return currentEvidence === false;
+  }
 }
--- src/app/ghost-properties.ts
+++ src/app/ghost-properties.ts
@@ -1,4 +1,4 @@
-export enum Ghosts {
+export enum Ghost {
   Phantom,
   Banshee,
   Mare,
@@ -22,17 +22,46 @@ export enum Evidence {
   Fingerprints
 }
 
-export const GHOST_EVIDENCE_MATRIX = [
-  [1, 1, 1, 0, 0, 0],
-  [1, 1, 0, 0, 0, 1],
-  [1, 0, 1, 1, 0, 0],
-  [1, 0, 1, 0, 1, 0],
-  [1, 0, 0, 1, 1, 0],
-  [1, 0, 0, 1, 0, 1],
-  [0, 1, 1, 1, 0, 0],
-  [0, 1, 1, 0, 1, 0],
-  [0, 1, 0, 1, 1, 0],
-  [0, 1, 0, 0, 1, 1],
-  [0, 0, 1, 1, 0, 1],
-  [0, 0, 0, 1, 1, 1],
+export interface NameDictionary {
+  id: number;
+  name: string;
+}
+
+export const GHOST: NameDictionary[] = [
+  {id: Ghost.Phantom, name: 'Phantom'},
+  {id: Ghost.Banshee, name: 'Banshee'},
+  {id: Ghost.Mare, name: 'Mare'},
+  {id: Ghost.Yurei, name: 'Yurei'},
+  {id: Ghost.Demon, name: 'Demon'},
+  {id: Ghost.Wraith, name: 'Wraith'},
+  {id: Ghost.Jinn, name: 'Jinn'},
+  {id: Ghost.Shade, name: 'Shade'},
+  {id: Ghost.Oni, name: 'Oni'},
+  {id: Ghost.Revenant, name: 'Revenant'},
+  {id: Ghost.Poltergeist, name: 'Poltergeist'},
+  {id: Ghost.Spirit, name: 'Spirit'},
 ];
+
+export const EVIDENCE: NameDictionary[] = [
+  {id: Evidence.Freezing, name: 'Freezing Temperatures'},
+  {id: Evidence.Emf, name: 'EMF Level 5'},
+  {id: Evidence.GhostOrbs, name: 'Ghost Orbs'},
+  {id: Evidence.SpiritBox, name: 'Spirit Box'},
+  {id: Evidence.GhostWriting, name: 'Ghost Writing'},
+  {id: Evidence.Fingerprints, name: 'Fingerprints'}
+];
+
+export const GHOST_EVIDENCE = {
+  [Ghost.Phantom]: [Evidence.Emf, Evidence.Freezing, Evidence.GhostOrbs],
+  [Ghost.Banshee]: [Evidence.Emf, Evidence.Freezing, Evidence.Fingerprints],
+  [Ghost.Mare]: [Evidence.Freezing, Evidence.GhostOrbs, Evidence.SpiritBox],
+  [Ghost.Yurei]: [Evidence.Freezing, Evidence.GhostOrbs, Evidence.GhostWriting],
+  [Ghost.Demon]: [Evidence.Freezing, Evidence.SpiritBox, Evidence.GhostWriting],
+  [Ghost.Wraith]: [Evidence.Freezing, Evidence.SpiritBox, Evidence.Fingerprints],
+  [Ghost.Jinn]: [Evidence.Emf, Evidence.SpiritBox, Evidence.GhostOrbs],
+  [Ghost.Shade]: [Evidence.Emf, Evidence.GhostOrbs, Evidence.GhostWriting],
+  [Ghost.Oni]: [Evidence.Emf, Evidence.SpiritBox, Evidence.GhostWriting],
+  [Ghost.Revenant]: [Evidence.Emf, Evidence.Fingerprints, Evidence.GhostWriting],
+  [Ghost.Poltergeist]: [Evidence.Fingerprints, Evidence.SpiritBox, Evidence.GhostOrbs],
+  [Ghost.Spirit]: [Evidence.Fingerprints, Evidence.SpiritBox, Evidence.GhostWriting]
+};
